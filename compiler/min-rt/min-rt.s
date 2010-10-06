3528428f1ff85bf48b0ec51198bb4910786d851d
pandora2000 <tetsuri.moriya@gmail.com>
2010-10-07 03:09:22 +0900

    クロージャ変換を出力してみた

---
+++ compiler/min-rt/min-rt.s
@@ -0,0 +1,78 @@
+xor.1977
+fsqr.1980
+fhalf.1982
+o_texturetype.1984
+o_form.1986
+o_reflectiontype.1988
+o_isinvert.1990
+o_isrot.1992
+o_param_a.1994
+o_param_b.1996
+o_param_c.1998
+o_param_x.2000
+o_param_y.2002
+o_param_z.2004
+o_diffuse.2006
+o_hilight.2008
+o_color_red.2010
+o_color_green.2012
+o_color_blue.2014
+o_param_r1.2016
+o_param_r2.2018
+o_param_r3.2020
+normalize_vector.2022
+sgn.2025
+rad.2027
+read_environ.2029
+read_nth_object.2031
+read_object.2033
+read_all_object.2035
+read_net_item.2037
+read_or_network.2039
+read_and_network.2041
+read_parameter.2043
+solver_rect.2045
+solver_surface.2048
+in_prod_sqr_obj.2051
+in_prod_co_objrot.2054
+solver2nd_mul_b.2057
+solver2nd_rot_b.2060
+solver_second.2063
+solver.2066
+is_rect_outside.2070
+is_plane_outside.2072
+is_second_outside.2074
+is_outside.2076
+check_all_inside.2078
+shadow_check_and_group.2081
+shadow_check_one_or_group.2085
+shadow_check_one_or_matrix.2089
+solve_each_element.2093
+solve_one_or_network.2096
+trace_or_matrix.2099
+tracer.2102
+get_nvector_rect.2105
+get_nvector_plane.2107
+get_nvector_second_norot.2109
+get_nvector_second_rot.2112
+get_nvector.2115
+utexture.2118
+in_prod.2121
+accumulate_vec_mul.2124
+raytracing.2128
+write_rgb.2131
+write_ppm_header.2133
+scan_point.2135
+scan_line.2137
+scan_start.2139
+rt.2141
+Let(Ti171.2145 : Int)
+ Int(768)
+In
+ Let(Ti172.2146 : Int)
+  Int(768)
+ In
+  Let(Ti173.2147 : Int)
+   Int(0)
+  In
+   AppDir(rt.2141, (Ti171.2145, Ti172.2146, Ti173.2147))
